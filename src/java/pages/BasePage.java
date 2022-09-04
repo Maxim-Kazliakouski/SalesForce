5a7881e7c64b8ba9c6a08c2edb8d6b41d9f3fded
kozlyakovskiyma <maxim.kazliakouski@gmail.com>
2022-09-04 17:20:22 +0300

    add Base page

---
+++ src/java/pages/BasePage.java
@@ -0,0 +1,53 @@
+package pages;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public abstract class BasePage {
+    WebDriver browser;
+    static WebDriverWait wait;
+
+
+    public BasePage(WebDriver browser) {
+        this.browser = browser;
+        wait = new WebDriverWait(browser, Duration.ofSeconds(15));
+    }
+
+    public boolean waitForVisibility(By locator) {
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            return true;
+        } catch (TimeoutException ex) {
+            return false;
+        }
+    }
+
+    public static boolean waitForElementClickable(WebElement webElement) {
+        try {
+            wait.until(ExpectedConditions.elementToBeClickable(webElement));
+            return true;
+        } catch (TimeoutException ex) {
+            return false;
+        }
+    }
+
+    public static void waitForPageLoaded() {
+        new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString().equals("complete");
+            }
+        };
+    }
+
+    public void changeExplicitlyWait(int timeOut) {
+        wait = new WebDriverWait(browser, Duration.ofSeconds(timeOut));
+    }
+
+    public void clickJS(By locator) {
+        ((JavascriptExecutor) browser).executeScript("arguments[0].click();", browser.findElement(locator));
+    }
+}
