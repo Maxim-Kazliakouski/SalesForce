c7dcc4ea331f55f1c13531a42d615b74e946b982
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-09-06 12:28:59 +0300

    add methods

--- src/java/pages/CreateContactPage.java
+++ src/java/pages/CreateContactPage.java
@@ -2,24 +2,29 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import tests.wrappers.ButtonAtCreateUserPage;
 import tests.wrappers.Dropdown;
 import tests.wrappers.Input;
-
+import tests.wrappers.Textarea;
 import java.io.IOException;
 
 public class CreateContactPage extends BasePage {
     private final By TITLE_CREATE_CONTACT_PAGE = By.xpath("//h2[text()='New Contact']");
+    private final By TOAST_MESSAGE = By.xpath("//span[@class='toastMessage slds-text-heading--small forceActionsText']");
+    public static By CREATED_CONTACT_NAME = By.xpath("//span[@class='custom-truncate uiOutputText']");
 
     public CreateContactPage(WebDriver browser) {
         super(browser);
     }
 
-    public void createContact(String firstName, String lastName, String phone, String salutationOption, String leadSourceOption) throws IOException, InterruptedException {
+    public void createContact(String firstName, String lastName, String phone, String salutationOption, String leadSourceOption, String text) throws IOException, InterruptedException {
         new Input(browser, "First Name").write(firstName);
         new Input(browser, "Last Name").write(lastName);
         new Input(browser, "Phone").write(phone);
         new Dropdown(browser, "Salutation").select(salutationOption);
         new Dropdown(browser, "Lead Source").select(leadSourceOption);
+        new Textarea(browser, "Mailing Street").input(text);
+        new ButtonAtCreateUserPage(browser,"Save").click();
     }
 
     public void open() {
@@ -30,4 +35,23 @@ public class CreateContactPage extends BasePage {
     public boolean isOpened() {
         return waitForVisibility(TITLE_CREATE_CONTACT_PAGE);
     }
+
+    public boolean isToastMessageAppeared(){
+        return waitForVisibility(TOAST_MESSAGE);
+    }
+
+    public String getToastMessageText(){
+        System.out.println(browser.findElement(TOAST_MESSAGE).getText());
+        return browser.findElement(TOAST_MESSAGE).getText();
+    }
+
+    public static boolean isContactNameAppeared(){
+        return waitForVisibility(CREATED_CONTACT_NAME);
+    }
 }
+
+
+
+
+
+
