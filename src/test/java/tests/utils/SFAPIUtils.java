27e4387978e8f604c3df6f86227a0cd34a3c0887
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-09-15 17:47:21 +0300

    adding SFAPIUtils

---
+++ src/test/java/tests/utils/SFAPIUtils.java
@@ -0,0 +1,63 @@
+package tests.utils;
+
+import okhttp3.*;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.Cookie;
+import org.testng.Assert;
+import pages.LoginPage;
+import tests.base.BaseTest;
+
+import java.io.IOException;
+import java.util.Date;
+
+import static java.lang.String.format;
+
+public class SFAPIUtils extends BaseTest {
+    public static String accessToken() throws IOException, ParseException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+                .url("https://login.salesforce.com/services/oauth2/token?grant_type=password&" +
+                        "client_id=" + LoginPage.getClientID() +
+                        "&client_secret=" + LoginPage.getClientSecret() +
+                        "&password=M@xkaz1991" +
+                        "&username=chrome@oxa.sandbox")
+                .method("POST", body)
+                .addHeader("Cookie", "CookieConsentPolicy=0:0; LSKey-c$CookieConsentPolicy=0:0")
+                .build();
+        Response response = client.newCall(request).execute();
+        String s = response.body().string();
+        // read json
+        Object json = new JSONParser().parse(s);
+        // Customize json into JSONObject
+        JSONObject jsonObject = (JSONObject) json;
+        // Get accessToken
+        return (String) jsonObject.get("access_token");
+    }
+
+    public static void deleteContact(String idContact) throws IOException, ParseException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+                .url("https://oxagile-dev-ed.my.salesforce.com/services/data/v52.0/sobjects/Contact/" + idContact + "")
+                .method("DELETE", body)
+                .addHeader("Authorization", "Bearer " + SFAPIUtils.accessToken() + "")
+                .addHeader("Cookie", "BrowserId=k_j6AiyKEe20rYmjFe7Zzg; CookieConsentPolicy=0:1; LSKey-c$CookieConsentPolicy=0:1")
+                .build();
+        Response response = client.newCall(request).execute();
+        Assert.assertEquals(response.code(), 204, format("The contact with id '%s' hasn't been deleted or something went wrong!", idContact));
+    }
+
+    public void setCookie(String sidValue) {
+        String LOGIN_URL = "https://oxagile-dev-ed.my.salesforce.com/";
+        browser.get(LOGIN_URL);
+        org.openqa.selenium.Cookie cookie = new Cookie("sid", sidValue, "/", (new Date(System.currentTimeMillis() * 1000L)));
+        browser.manage().addCookie(cookie);
+    }
+}
