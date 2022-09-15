985896e7f694a902170c68bf40926df7fe83eb57
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-09-15 17:44:22 +0300

    deleting unused methods

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,17 +2,24 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.Assert;
 
 public class LoginPage extends BasePage {
     private final By ERROR_MISSED_PASSWORD = By.id("error");
     private final By TEXT_CHECKBOX_REMEMBER_ME = By.xpath("//label[text()='Remember me']");
     private final By CHECKBOX_REMEMBER_ME = By.id("rememberUn");
     private final By FORGOT_PASSWORD_LINK = By.xpath("//a[text()='Forgot Your Password?']");
-    private final By USERNAME = By.id("username");
-    private final By PASSWORD = By.id("password");
-    private final By LOGIN_BUTTON = By.id("Login");
+    public static final By USERNAME = By.id("username");
+    public static final By PASSWORD = By.id("password");
+    public static final By LOGIN_BUTTON = By.id("Login");
+
+    public static String getClientID() {
+        return "3MVG9fe4g9fhX0E5qL4iy8FRVTFbj4GiI1_XIxZT1LuG4cX1MTDHGmR9YAg.or2_VnWf6_jJtseR1_QOYFe88";
+    }
+
+    public static String getClientSecret() {
+        return "4BB30BC4D932875F595B9E26F3125732351400BF83491DB91EFCC30C968B43D7";
+    }
 
     public LoginPage(WebDriver browser) {
         super(browser);
@@ -28,7 +35,7 @@ public class LoginPage extends BasePage {
     }
 
     public void clickForgotPasswordLink() {
-        click(FORGOT_PASSWORD_LINK);
+        browser.findElement(FORGOT_PASSWORD_LINK).click();
     }
 
     public String getErrorMessage() {
@@ -49,9 +56,11 @@ public class LoginPage extends BasePage {
         return waitForVisibility(FORGOT_PASSWORD_LINK);
     }
 
-    public void signUpForNegative(String username, String password) {
-        browser.findElement(USERNAME).sendKeys(username);
-        browser.findElement(PASSWORD).sendKeys(password);
-        browser.findElement(LOGIN_BUTTON).click();
+    public void signUp(String username, String password) {
+        open();
+        Assert.assertTrue(isOpened(), "Login page wasn't opened!");
+        browser.findElement(LoginPage.USERNAME).sendKeys(username);
+        browser.findElement(LoginPage.PASSWORD).sendKeys(password);
+        browser.findElement(LoginPage.LOGIN_BUTTON).click();
     }
 }
