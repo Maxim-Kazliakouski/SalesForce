b085efe36d3727817200135d39e766e87f5a312c
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-09-29 15:48:04 +0300

    adding all tests

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -14,48 +14,48 @@ public class LoginPageTest extends BaseWithStepsTest {
         };
     }
 
-//    @Test(description = "successful signUp with correct login data",
-//            groups = "valid login")
-//    public void successfulSignUp() {
-//        loginSteps
-//                .open()
-//                .login(getUsername(), getPassword());
-//    }
-//
-//    @Test(dataProvider = "loginData",
-//            description = "Unsuccessful signUp with invalid data",
-//            groups = "unsuccessful login")
-//    public void unsuccessfulSignUp(String invalidUsername, String invalidPassword, String error) {
-//        loginSteps
-//                .open()
-//                .loginWithInvalidData(invalidUsername, invalidPassword)
-//                .getErrorMessage(error);
-//    }
-//
-//    @Test(description = "Is 'Remember me' checkbox displayed at the Login page",
-//            groups = "unsuccessful login")
-//    public void checkBoxRememberMe() {
-//        loginSteps
-//                .open()
-//                .isCheckboxDisplayed();
-//    }
-//
-//    @Test(description = " is 'Remember me' checkbox unchecked at the Login page",
-//            groups = "unsuccessful login")
-//    public void uncheckedRememberMeCheckbox() {
-//        loginSteps
-//                .open()
-//                .isCheckboxUnchecked();
-//    }
-//
-//    @Test(description = "is 'Forgot Your Password' link displayed at the Login page",
-//            groups = "unsuccessful login")
-//    public void forgotPasswordLink() {
-//        loginSteps
-//                .open()
-//                .isForgotPasswordLinkDisplayed();
-//    }
-//
+    @Test(description = "successful signUp with correct login data",
+            groups = "valid login")
+    public void successfulSignUp() {
+        loginSteps
+                .open()
+                .login(getUsername(), getPassword());
+    }
+
+    @Test(dataProvider = "loginData",
+            description = "Unsuccessful signUp with invalid data",
+            groups = "unsuccessful login")
+    public void unsuccessfulSignUp(String invalidUsername, String invalidPassword, String error) {
+        loginSteps
+                .open()
+                .loginWithInvalidData(invalidUsername, invalidPassword)
+                .getErrorMessage(error);
+    }
+
+    @Test(description = "Is 'Remember me' checkbox displayed at the Login page",
+            groups = "unsuccessful login")
+    public void checkBoxRememberMe() {
+        loginSteps
+                .open()
+                .isCheckboxDisplayed();
+    }
+
+    @Test(description = " is 'Remember me' checkbox unchecked at the Login page",
+            groups = "unsuccessful login")
+    public void uncheckedRememberMeCheckbox() {
+        loginSteps
+                .open()
+                .isCheckboxUnchecked();
+    }
+
+    @Test(description = "is 'Forgot Your Password' link displayed at the Login page",
+            groups = "unsuccessful login")
+    public void forgotPasswordLink() {
+        loginSteps
+                .open()
+                .isForgotPasswordLinkDisplayed();
+    }
+
     @Test(description = "redirection to the forgot password page",
             groups = "unsuccessful login")
     public void redirectionForgotPage() {
