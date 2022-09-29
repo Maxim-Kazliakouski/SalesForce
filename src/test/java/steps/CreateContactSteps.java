270a8c05c3b4a9c0985dc133e195534285c24766
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-09-29 12:40:26 +0300

    adding Steps for CreateContact

---
+++ src/test/java/steps/CreateContactSteps.java
@@ -0,0 +1,40 @@
+package steps;
+
+import dto.Contact;
+import factories.ContactFactory;
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.WebDriver;
+import pages.CreateContactPage;
+
+import static java.lang.String.format;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+@Log4j2
+public class CreateContactSteps {
+    CreateContactPage createContactPage;
+
+    public CreateContactSteps(WebDriver browser) {
+        createContactPage = new CreateContactPage(browser);
+    }
+
+    @Step("Go to the create contact page")
+    public CreateContactSteps goToCreateContactPage() {
+        createContactPage.open();
+        log.info("Opening 'Create contact' page");
+        assertTrue(createContactPage.isOpened(), "Create new user page wasn't open");
+        return this;
+    }
+
+    public void createNewContact() {
+        Contact contact = ContactFactory.get();
+        log.info("Creating new contact");
+        createContactPage.createContact(contact);
+        assertTrue(createContactPage.isToastMessageAppeared(),
+                "The confirmation message about success created user wasn't appeared!");
+        log.info(format("New contact '%s%s%s' has been created", contact.getSalutation(), contact.getFirstName(), contact.getLastName()));
+        assertEquals(createContactPage.getToastMessageText(),
+                "Contact \"" + contact.getSalutation() + " " + contact.getFirstName() + " " + contact.getLastName() + "\" was created.");
+    }
+}
