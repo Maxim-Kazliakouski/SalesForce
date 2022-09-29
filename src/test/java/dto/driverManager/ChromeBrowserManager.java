e909884f52e2d1052cf61b1b2b52cd332a14dbf0
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-09-29 12:35:08 +0300

    adding ChromeBrowserManager

---
+++ src/test/java/dto/driverManager/ChromeBrowserManager.java
@@ -0,0 +1,19 @@
+package dto.driverManager;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeBrowserManager extends BrowserManager {
+
+    @Override
+    public void createBrowser(String headlessMode) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=1920x1080");
+        options.addArguments("--disable-notifications");
+        options.setHeadless(headlessMode.equals("true"));
+        browser = new ChromeDriver(options);
+        browser.manage().deleteAllCookies();
+    }
+}
